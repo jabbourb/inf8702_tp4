17402d9502e1cd95b04a9abd5db97297d39bf69c
Bureau <Bureau@DESKTOP-V4P9B8C>
2016-11-01 10:12:37 -0400

    Profondeur des champs naive

--- Nuanceurs/blurFXFragments.glsl
+++ Nuanceurs/blurFXFragments.glsl
@@ -86,26 +86,27 @@ void main (void)
    
 		// Quantifier la différence de profondeur entre le point visé et le fragment courant:
 		// Utilisez LineariserProfondeur() sur les profondeurs échantillonnées
-		// depth = ... 
-		// center_depth =  ... 
+		depth = LineariserProfondeur(texture(depthMap, fragTexCoord.xy).x); 
+		
+		center_depth = LineariserProfondeur(texture(depthMap, vec2(0.5,0.5)).x); 
 
 		// Le poids est simplement la différence absolue entre ces deux valeurs:
-		// w= ...
+		w= abs(depth - center_depth);
 
 		// Appliquer un filtre gaussien (étendue de 1) :
-		// blurred_col = ...
+		blurred_col = FiltreGaussien(fragTexCoord.x,fragTexCoord.y,1);
 		// Appliquer un filtre gaussien avec une plus grande étendue (ex.: 5) :
-		// blurred_col += ...
+		blurred_col += FiltreGaussien(fragTexCoord.x,fragTexCoord.y,5);
 		// Moyenne des deux filtres :
-		// blurred_col = ...
+		blurred_col = blurred_col/2;
 
 		// À modifier :
 		// Ajuster la couleur selon la différence de profondeur entre le point visé et le fragment courant:
-		color = clear_color;
+		color =  w * blurred_col + (1 - w) * clear_color;
 		
 
 		// Afin de déboguer, on peut affichier simplement les valeurs de profondeurs:
-		// color = vec4(depth, depth, depth, 1.0);
+		 //color = vec4(depth, depth, depth, 1.0);
 	
 	}
     
